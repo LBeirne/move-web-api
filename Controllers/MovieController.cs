f1f18957d9479742f197869b97292758378ae9db
LBeirne <lpbeirne@coastal.edu>
2023-03-22 14:12:15 -0400

    added MovieController.cs, removed oh brother

---
+++ Controllers/MovieController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace movie_web_api.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MovieController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<MovieController> _logger;
+
+        public MovieController(ILogger<MovieController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<Movie> Get()
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new Movie
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
